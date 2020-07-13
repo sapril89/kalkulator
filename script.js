c2d6cfefb0254dd016e4f665fd5e2b23907b1a8c
sapril89 <67051241+sapril89@users.noreply.github.com>
2020-07-14 03:11:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,69 @@
-const numbers = document.querySelectorAll(".number")
+const calculatorScreen = document.querySelector('.calculator-screen');
+
+const updateScreen = (number) => {
+	calculatorScreen.value = number;
+}
+
+const inputNumber = (number) => {
+	if (currentNumber === '0') {
+		currentNumber = number;
+	} else {
+		currentNumber += number;
+	}
+}
+
+const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) => {
-	number.addEventListener("click", () => {
-		console.log("number is pressed")
-	})
+	number.addEventListener("click", (event) => {
+		inputNumber(event.target.value);
+		updateScreen(currentNumber);
+	});
+});
+
+let prevNumber = '';
+let calculationOperator = '';
+let currentNumber = '0';
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+	operator.addEventListener("click", (event) => {
+		inputOperator(event.target.value);
+	});
+});
+
+const inputOperator = (operator) => {
+	prevNumber = currentNumber;
+	calculationOperator = operator;
+	currentNumber = '';
+}
+
+const equalSign = document.querySelector('.equal-sign');
+
+equalSign.addEventListener('click', () => {
+	calculate();
+	updateScreen(currentNumber);
 })
+
+const calculate = () => {
+	let result = ''
+	switch (calculationOperator) {
+		case '+':
+			result = prevNumber + currentNumber
+			break
+		case '-'
+			result = prevNumber - currentNumber
+			break
+		case '*'
+			result = prevNumber * currentNumber
+			break
+		case "/"
+			result = prevNumber / currentNumber
+			break
+		default:
+			return
+	}
+}
+currentNumber = result;
+calculationOperatorn = '';
