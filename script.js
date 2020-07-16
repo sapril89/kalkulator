728bc23b990fae53d514ee3048d9188215f70c23
sapril89 <67051241+sapril89@users.noreply.github.com>
2020-07-16 16:18:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,9 +34,11 @@ operators.forEach((operator) => {
 });
 
 const inputOperator = (operator) => {
-	prevNumber = currentNumber;
+	if (calculationOperator === '') {
+		prevNumber = currentNumber;
+	}
 	calculationOperator = operator;
-	currentNumber = '';
+	currentNumber = '0';
 }
 
 const equalSign = document.querySelector('.equal-sign');
